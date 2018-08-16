f50f3590f4b7b9434de7a4a81920b166a628e83b
Shakurri <puz,show40716@gmail.com>
2018-08-16 21:34:32 +0900

    add10

--- app/assets/javascripts/TaskAnimation.js
+++ app/assets/javascripts/TaskAnimation.js
@@ -10,43 +10,41 @@ $(function(){
     $(".message_del").slideUp(600);
     $(".message_del_but").slideUp(600);
   }).css('cursor','pointer');
-})
 
-$(function(){
+
+
   $('.icon_delate').on('touchstart',function(){
     $(".message_del").slideDown(600);
     $(".message_del_but").slideDown(600);
   }).css('cursor','pointer');
-})
 
-$(function(){
+
+
   $('.slidein').on('touchstart',function(){
     $(".header-form-container").slideUp(600);
     $(".input").slideDown();
     $(".icon_slide").fadeOut(500);
     $(".bury_box").fadeOut(500);
   }).css('cursor','pointer');
-})
 
-$(function(){
+
 $('.icon_fade').on('touchstart',function(){
     $(".capsule").fadeOut(500);
     $(".icon_delate").slideUp();
     $(".message_del").slideUp(600);
     $(".message_del_but").slideUp(600);
   }).css('cursor','pointer');
-})
 
-$(function(){
+
+
   $('.bury').on('touchstart',function(){
     $(".bury_box").fadeTo(300,0.1);
     $(".header-form-container").slideUp(1000);
     $(".input").slideDown();
     $(".icon_slide").fadeOut(500);
   }).css('cursor','pointer');
-})
 
-$(function(){
+
   var match = location.search.match(/id=(.*?)(&|$)/);
   console.log(match);
 
@@ -64,9 +62,9 @@ $(function(){
     $(".message_del").slideUp(600);
     $(".message_del_but").slideUp(600);
   }).css('cursor','pointer');
-})
 
-$(function(){
+
+
   $('.but_bury').on('touchstart',function(){
     $(".bury_box").fadeTo(300,0.1);
     $(".header-form-container").slideUp(1000);
@@ -79,9 +77,8 @@ $(function(){
     $(".backblack").fadeIn();
     $(".past_box").fadeIn(1500);
   }).css('cursor','pointer');
-})
 
-$(function(){
+
   $('.rec_but').on('touchstart',function(){
     $(".past_box").fadeOut(600);
     $(".backblack").fadeOut(1000);
